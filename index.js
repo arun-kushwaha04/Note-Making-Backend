57758d0fab0f81382d1cfd545fba85fa44158fd7
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-05-25 11:06:48 +0530

    Request Origin HEders added

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const userRoute = require('./routes/profile');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.json());
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
+app.use(express.json());
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type");
+//     next();
+// });
 
 //connecting to database
 client.connect(err => {
